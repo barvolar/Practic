2d0fcce11f7fc68ae4bdf7c85ed746226647c697
barvolar <kostam96@yandex.ru>
2021-09-15 13:45:30 +0300

    2

--- Polycliniс/Polycliniс/Program.cs
+++ Polycliniс/Polycliniс/Program.cs
@@ -7,13 +7,14 @@ namespace Polycliniс
         static void Main(string[] args)
         {
             int receptionTime = 10;
+            int oneHour = 60;
 
             Console.WriteLine("Сколько в очереди людей ?");
             int countHuman=Convert.ToInt32(Console.ReadLine());
 
             int totalReceptionTime = receptionTime * countHuman;
-            int hourReception = totalReceptionTime / 60;
-            int minutReception = totalReceptionTime % 60;
+            int hourReception = totalReceptionTime / oneHour;
+            int minutReception = totalReceptionTime % oneHour;
 
             Console.WriteLine("Вам предстоит ожидать " + hourReception + " часов и " + minutReception + " минут ");
         }
---
+++ String/String/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как вас зовут ?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Сколько вам лет ?");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("Кем вы работаете ?");
+            string profession = Console.ReadLine();
+
+            Console.WriteLine("Где вы живёте ?");
+            string placeLive = Console.ReadLine();
+
+            Console.WriteLine("Кто вы по знаку задиака?");
+            string zodiak = Console.ReadLine();
+
+            Console.WriteLine("Как вас зовут?");
+        }
+    }
+}
