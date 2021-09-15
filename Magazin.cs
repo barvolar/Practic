ee7ad23887d67ff4cc233ba88f0cfc49ee67048e
barvolar <kostam96@yandex.ru>
2021-09-15 10:30:46 +0300

    переделанное

---
+++ Magazin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		int priceCrystal=5;
+
+		Console.WriteLine("Введите количество ваших золотых");
+		int myGold = Convert.ToInt32(Console.ReadLine());
+		
+		Console.WriteLine("Здравствуйте , сколько кристалов вы хотите купить?");
+		int crystalCount = Convert.ToInt32(Console.ReadLine());
+
+		Console.WriteLine("Вы купили " + crystalCount * priceCrystal + " и у вас осталось " + myGold - priceCrystal * crystalCount);
+
+	}
+}
--- Variables.cs
+++ Variables.cs
@@ -5,19 +5,17 @@ public class Class1
 {
 	public Class1()
 	{
-        nt myLevel = 10;
+        int myLevel = 10;
         float healthPoint = 345.2f;
-        double myExp = 87432.543;
-        decimal limit Of Experience = 10000 * my Level;
+        double myExperience = 87432.543;
+        decimal limitOfExperience = 10000 * myLevel;
         string color Door = "Red";
         char color Key = 'R';
-        bool isfacingright = true;
+        bool isFacingRight = true;
         byte level Danger = 1;
         short damage = 667;
-        int b = 7;
-        int a = b - 24;
         long armor = 334;
-        Console.WriteLine(a);
+        
 
         
     }
