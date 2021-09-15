7636cc5bf359b50326299cd0f1b7d77dc80766c2
barvolar <kostam96@yandex.ru>
2021-09-15 15:00:45 +0300

    1

---
+++ Cikl1/Cikl1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Cikl1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxBurningTime = 3;
+            int myBurningTime=0;
+
+            while (myBurningTime < maxBurningTime)
+            {
+                Console.WriteLine("Вы горите");
+                myBurningTime++;
+            }
+        }
+    }
+}
