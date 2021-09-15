a716429c49f619a5d153d9378a10dd344a20f8e5
barvolar <kostam96@yandex.ru>
2021-09-15 13:37:10 +0300

    q

---
+++ Polycliniс/Polycliniс/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Polycliniс
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int receptionTime = 10;
+
+            Console.WriteLine("Сколько в очереди людей ?");
+            int countHuman=Convert.ToInt32(Console.ReadLine());
+
+            int totalReceptionTime = receptionTime * countHuman;
+            int hourReception = totalReceptionTime / 60;
+            int minutReception = totalReceptionTime % 60;
+
+            Console.WriteLine("Вам предстоит ожидать " + hourReception + " часов и " + minutReception + " минут ");
+        }
+    }
+}
