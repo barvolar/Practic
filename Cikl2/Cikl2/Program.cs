cffef6b0bfc27cf46bbba0caac64230b73a73ec6
barvolar <kostam96@yandex.ru>
2021-09-15 15:09:43 +0300

    1

---
+++ Cikl2/Cikl2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cikl2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string exitKey = "Exit";
+            string myKey;
+            Console.WriteLine("напишите выход");
+            do
+            {
+                Console.WriteLine("неверное!!!");
+                myKey = Console.ReadLine();               
+            } 
+            while (myKey!=exitKey);
+        }
+    }
+}
