b5f5c6819f2479a607222245be6117e8f209cd1e
barvolar <kostam96@yandex.ru>
2021-09-15 07:59:05 +0300

    переменные

---
+++ Variables.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        nt myLevel = 10;
+        float healthPoint = 345.2f;
+        double myExp = 87432.543;
+        decimal limit Of Experience = 10000 * my Level;
+        string color Door = "Red";
+        char color Key = 'R';
+        bool isfacingright = true;
+        byte level Danger = 1;
+        short damage = 667;
+        long armor = 334;
+
+    }
+}
