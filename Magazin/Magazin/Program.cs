60f7ea8bbbcdb725338953bd0b4fbe81e8d942d3
barvolar <kostam96@yandex.ru>
2021-09-15 12:58:18 +0300

    1

---
+++ Magazin/Magazin/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Magazin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int crystalPrice = 5;
+
+            Console.WriteLine("Сколько у вас золота ?");
+            int myGold = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Сколько кристалов хотите купить ? Один кристал стоит "+crystalPrice+" золотых");
+            int crystalCount = Convert.ToInt32(Console.ReadLine());
+
+            int totalPrice = crystalPrice * crystalCount;
+            myGold -= totalPrice;
+            
+            Console.WriteLine("вы купили "+crystalCount+" кристалов и у вас осталось "+myGold+" золота");
+        }
+    }
+}
