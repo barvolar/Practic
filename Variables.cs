439ee86afe06f3ab169e929cfc7f701f27065767
barvolar <kostam96@yandex.ru>
2021-09-15 10:39:37 +0300

    as

--- Variables.cs
+++ Variables.cs
@@ -9,10 +9,10 @@ public class Class1
         float healthPoint = 345.2f;
         double myExperience = 87432.543;
         decimal limitOfExperience = 10000 * myLevel;
-        string color Door = "Red";
-        char color Key = 'R';
+        string colorDoor = "Red";
+        char colorKey = 'R';
         bool isFacingRight = true;
-        byte level Danger = 1;
+        byte levelDanger = 1;
         short damage = 667;
         long armor = 334;
         
