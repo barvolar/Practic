2e65d4b5d225b9502a223a702ad081c06a083a7e
barvolar <kostam96@yandex.ru>
2021-09-16 09:32:00 +0300

    2

--- Cikl4/Cikl4/Program.cs
+++ Cikl4/Cikl4/Program.cs
@@ -17,6 +17,10 @@ namespace Cikl4
             {
                 switch (userInput)
                 {
+                    case "0":
+                        Console.WriteLine("Finalo4ka");
+                        userInput = "Exit";
+                        break;
                     case "1":
                         Console.WriteLine("Введите ваше имя");
                         name = Console.ReadLine();
@@ -36,7 +40,7 @@ namespace Cikl4
                         userInput = Console.ReadLine();
                         break;
                     case "4":
-                        Console.WriteLine(" Name = " + name + "\n Age = " + age + " \n Password = "+password );
+                        Console.WriteLine(" Name = " + name + "\n Age = " + age + " \n Password = " + password);
                         Console.WriteLine("\n 1 = Set Name \n 2 = Set Age \n 3 = Set Password \n 4 = Check Information");
                         userInput = Console.ReadLine();
                         break;
