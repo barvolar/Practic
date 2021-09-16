ee55c024c56af32f9847979e01e2249c015a5f1c
barvolar <kostam96@yandex.ru>
2021-09-16 13:49:02 +0300

    1

---
+++ Cikl7/Cikl7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Cikl7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "113";
+            string userInput;
+            int tryCount = 3;
+            int tryInput = 0;
+            Console.WriteLine("Введите пароль");
+            while (tryInput != tryCount)
+            {
+                userInput = Console.ReadLine();
+                if (userInput == password)
+                {
+                    Console.WriteLine("TikTok zlo");
+                    break;
+                }
+                else
+                {
+                    tryInput++;
+                    Console.WriteLine("Неверно , осталось "+(tryCount-tryInput)+" попыток");
+                }
+            }
+
+
+        }
+    }
+}
