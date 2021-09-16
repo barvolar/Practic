1f45b91d2861ef0a9a11df99bdf550e6b943b48b
barvolar <kostam96@yandex.ru>
2021-09-16 13:06:32 +0300

    1

---
+++ Cikl5/Cikl5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Cikl5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 7; i < 100; i+=7 )
+            {
+                Console.WriteLine(i);                
+            }
+        }
+    }
+}
