c8e8ef4cf0ee6755073ffdb6079fc2a8a7812f14
barvolar <kostam96@yandex.ru>
2021-09-20 21:31:44 +0300

    5

--- CONSOlgame/CONSOlgame/Program.cs
+++ CONSOlgame/CONSOlgame/Program.cs
@@ -8,7 +8,7 @@ namespace CONSOlgame
         {
             //Дан двумерный массив.
             //Вычислить сумму второй строки и произведение первого столбца. Вывести исходную матрицу и результаты вычислений.
-            int[,] arrays = {{1,2,3,4,5 },{1,3,5,7,9 },{2,4,6,8,2 },{5,6,7,8,9 }};
+            int[,] arrays = { { 1, 2, 3, 4, 5 }, { 1, 3, 5, 7, 9 }, { 2, 4, 6, 8, 2 }, { 5, 6, 7, 8, 9 } };
             int sum = 0;
             int multiply = 1;
 
@@ -19,7 +19,6 @@ namespace CONSOlgame
                     Console.Write(arrays[i, j] + " ");
                 }
                 Console.WriteLine();
-
             }
 
             for (int i = 0; i < arrays.GetLength(1); i++)
