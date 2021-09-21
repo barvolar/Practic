95bf9186c66aa2c6937316aa1d04d22e0ffcc54e
barvolar <kostam96@yandex.ru>
2021-09-21 10:30:31 +0300

    2

--- LocalMaximum/LocalMaximum/Program.cs
+++ LocalMaximum/LocalMaximum/Program.cs
@@ -4,14 +4,6 @@ namespace LocalMaximum
 {
     class Program
     {
-        /*Дан одномерный массив целых чисел из 30 элементов.
-Найдите все локальные максимумы и вывести их. (Элемент является локальным максимумом, если он не имеет соседей, больших, чем он сам)
-
-Крайние элементы являются локальными максимумами если не имеют соседа большего, чем они сами.
-
-Программа должна работать с массивом любого размера.
-
-Массив всех локальных максимумов не нужен.*/
         static void Main(string[] args)
         {
             int[] array = new int[30];
@@ -22,6 +14,7 @@ namespace LocalMaximum
                 array[i] = random.Next(1, 100);
                 Console.Write(array[i] + " ");
             }
+
             if (array[0] > array[1])
             {
                 Console.WriteLine("\nНайден локальный массив " + array[0]);
@@ -34,11 +27,11 @@ namespace LocalMaximum
                     Console.WriteLine("Найден локальный максимум " + array[i]);
                 }
             }
+
             if (array[array.Length - 1] > array[array.Length - 2])
             {
                 Console.WriteLine("Найден локальный максимум "+array[array.Length]);
             }
-
         }
     }
 }
