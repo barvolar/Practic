23b1cfd50d82d31facf91b7e2eea9715bbfb665a
barvolar <kostam96@yandex.ru>
2021-10-27 14:52:26 +0300

    2

--- AppDinamicArray/AppDinamicArray/Program.cs
+++ AppDinamicArray/AppDinamicArray/Program.cs
@@ -11,23 +11,26 @@ namespace AppDinamicArray
         static void Main(string[] args)
         {
             List<int> numbers = new List<int>();
-            string userInput=" ";
-            int sum=0;
+            string userInput = " ";
+            bool isPlay = true;
 
-            ShowMessage();
+            ShowMeny();
 
-            while (userInput != "Exit")
+            while (isPlay)
             {
                 userInput = Console.ReadLine();
                 switch (userInput)
                 {
                     case "Sum":
                         Console.Clear();
-                        ShowMessage();
-                        Console.WriteLine($"Сумма = {ShowList(numbers,sum)}");                       
+                        ShowMeny();
+                        Console.WriteLine($"Сумма = {ShowSum(numbers)}");
+                        break;
+                    case "Exit":
+                        isPlay = false;
                         break;
                     default:
-                        if (Int32.TryParse(userInput, out int i)) 
+                        if (Int32.TryParse(userInput, out int i))
                         {
                             numbers.Add(i);
                         }
@@ -36,12 +39,13 @@ namespace AppDinamicArray
                             Console.WriteLine("Ну разве это число ?");
                         }
                         break;
-                }          
+                }
             }
         }
 
-        static int ShowList(List<int> list,int resul)
+        static int ShowSum(List<int> list)
         {
+            int resul = 0;
             for (int i = 0; i < list.Count; i++)
             {
                 resul += list[i];
@@ -49,7 +53,7 @@ namespace AppDinamicArray
             return resul;
         }
 
-        static void ShowMessage()
+        static void ShowMeny()
         {
             Console.WriteLine("Введите числа. Когда надоест введите Sum и получите их сумму. \nЧто бы выйти введите Exit ");
         }
