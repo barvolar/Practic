9332775e297031705fee3fd82acfef16eba28c33
barvolar <kostam96@yandex.ru>
2021-11-03 21:16:05 +0300

    1

---
+++ ClassWork/ClassWork/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player enemyPlayer = new Player();
+            enemyPlayer.ShowStats();
+        }
+    }
+
+    class Player
+    {
+        private string _name = "Убицца328";
+        private string _profession = "Пекарь";
+        private int _health = 100;
+
+        public void ShowStats()
+        {
+            Console.Write($" Имя - {_name}\n Профессия - {_profession}\n Жизни - {_health}");
+        }
+    }
+}
