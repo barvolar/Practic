20e3337ae8cc90c37219d6c044bcc4e8a6ae2d15
barvolar <kostam96@yandex.ru>
2021-11-06 10:11:08 +0300

    Работа со свойствами

---
+++ Working with properties/Working with properties/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Working_with_properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player(13, 1);
+
+            Renderer showPlayer = new Renderer();
+
+            showPlayer.DrowPlayer(player1.X, player1.Y);
+
+        }
+    }
+
+    class Renderer
+    {
+        public void DrowPlayer(int x, int y, char player = '@')
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(player);
+        }
+    }
+
+    class Player
+    {
+        private int _x;
+        public int X
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                _x = value;
+            }
+        }
+        public int Y { get; private set; }
+
+        public Player(int x, int y)
+        {
+            _x = x;
+            Y = y;
+        }
+    }
+}
