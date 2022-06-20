c81e0f860b85ed54c2c6adb594a6388c5a0d0ced
barvolar <86926525+barvolar@users.noreply.github.com>
2022-06-20 13:06:39 +0300

    Test

--- Cikl1/Cikl1/Program.cs
+++ Cikl1/Cikl1/Program.cs
@@ -13,6 +13,10 @@ namespace Cikl1
             list.Add(1);
             list.Add(2);
             list.Add(3);
+            list.Add(4);
+            list.Add(5);
+
+            
 
             for (int i = 0; i < list.Count; i++)
             {
